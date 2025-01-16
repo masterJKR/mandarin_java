78b36518f84f4b8bab717814a9c310df987f9acc
masterJKR <nilzero25@gmail.com>
2025-01-16 12:50:20 +0900

    자바변수

--- java_study1/src/study0116/test.java
+++ java_study1/src/study0116/test.java
@@ -3,8 +3,31 @@ package study0116;
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// 주석- 설명이나 메모, 코드 대기, 등등 
+		/*
+		 *   여러줄 주석 - 주석은 프로그램으로 실행 되지않는 부분이다. 
+		 *   주석안에 코드 작성해도 실행 안된다.  각 코드의 설명을 작성 하는용도
+		 *   로 사용 하면 되요
+		 *   저장 단축키 - ctrl + s
+		 *   주석 단축키 - ctrl + /
+		 *   실행 단축키 - ctrl + f11
+		 */
+		System.out.println("자바 이클립스 테스트");
+			
+		// 데이터를 사용 하려면 변수를 선언 해야 한다.
+		// 변수를 선언 할때는 반드시 데이터 타입을 정해야한다.
+		// 변수의 이름은 저장할 데이터에 맞는 의미 있는 이름으로 짓기
+		// 정수 :  byte, short, int, long
+		// 실수 : float , double
+		// 문자 : char
+		// 논리 : boolean
+		int age; 
+		age = 100;  // age 변수에 100 저장(대입연산)
+		age = 27;
+		//age = 3.14f; // 자바에서 '' 표현은 문자를 의미 
+		           // "" 표현은 문자열을 의미
+		
+		
 	}
 
 }
