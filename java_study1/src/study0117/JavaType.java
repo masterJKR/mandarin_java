76a5775157525fee30ae2ec34360051fc311c992
masterJKR <nilzero25@gmail.com>
2025-01-20 12:10:33 +0900

    1월20일 과제

--- java_study1/src/study0117/JavaType.java
+++ java_study1/src/study0117/JavaType.java
@@ -4,15 +4,28 @@ public class JavaType {
 
 	public static void main(String[] args) {
 		
-		int num = 20;
-		float pi=3.14f;
+//		int num = 20;
+//		float pi=3.14f;
+//		
+//		pi = num; // 자동 형변환
+//		num = (int)pi;  //강제 형변환 - 일시적 타입 변환
+//		char ch = (char)pi;
+//		
+//		int a=2;
 		
-		pi = num; // 자동 형변환
-		num = (int)pi;  //강제 형변환 - 일시적 타입 변환
-		char ch = (char)pi;
+		int 한개피단축 = 2; // 2분 - 분단위
+		int 일년 = 365;
+		int 흡연기간 = 20;
+		int 하루담배 = 5; // 하루에 5개피씩 매일 흡연
 		
-		int a=2;
+		int 단축수명 = 하루담배 * 한개피단축 * 일년 * 흡연기간;
+		int 단축수명시간 = 단축수명/60;
+		int 단축수명일 = 단축수명시간/24;
 		
+		System.out.println("총 단축수명(분) : " + 단축수명);
+		System.out.println("총 단축수명(시간) : " + 단축수명시간);
+		System.out.println("총 단축수명(일) : " + 단축수명일);
+//		System.out.println("총" + 단축수명);
 	}
 
 }
@@ -27,4 +40,5 @@ public class JavaType {
 이사람의  단축된 수명이 총 몇분인가? 출력
 이사람의 단축된 수명이 총 몇 시간인가? 출력
 이 사람의 단축된 수명이 총 몇일 인가? 출력
+
 */
