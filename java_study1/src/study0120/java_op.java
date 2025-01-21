9a23cd46b1fb317cc8d34e027ede1c5385d76222
masterJKR <nilzero25@gmail.com>
2025-01-21 11:51:19 +0900

    1월 21일 - 입 출력

--- java_study1/src/study0120/java_op.java
+++ java_study1/src/study0120/java_op.java
@@ -20,12 +20,27 @@ public class java_op {
 		// 2. 입력 객체를 통해 데이터에 맞는 입력메서드 호출
 		
 		// Scanner 변수이름 = new Scanner(System.in);
-		Scanner input = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
 		// num 이라는 변수에 정수 데이터입력하여 저장하기
 		// nextInt() 가 정수 데이터 입력 하기 위한 메서드이다.
-		int num = input.nextInt(); 
+		System.out.print("정수 입력 : ");
+		int num = sc.nextInt();
 		
+		System.out.println( "입력 잘되었나?  "+ num );
+		
+		
+		//  사각형의 너비와 높이를 키보드를 통해 입력받고
+		//  넓이를 계산하여 출력하세요.
+		
+		int width=0 , height=0;
+		System.out.print("너비 입력 : ");
+		width = sc.nextInt();
+		System.out.print("높이 입력 : ");
+		height = sc.nextInt();
+		// 사각형 넓이 구하기
+		int area = width * height;
+		System.out.println("넓이 : "+ area);
 		
 
 	}
@@ -33,6 +48,20 @@ public class java_op {
 }
 
 /*
+ 	
+ 	int apple = 20;
+ 	int appleBox = 11;
+ 	int orange = 15;
+ 	int orangeBox = 12; 
+ 	
+ 	int totalApple = appleBox * apple;
+ 	int totalOrange = orangeBox * orange;
+ 	int totalFruit = totalApple + totalOrange;
+ 	System.out.println("사과 총 갯수 : " + totalApple);
+ 	System.out.println("오렌지 총 갯수 : " + totalOrange);
+ 	System.out.println("과일 총 갯수 : " + totalFruit);
+ 	
+ 	
  	1월 20일 과제
  	(변수 활용 문제)
  	
