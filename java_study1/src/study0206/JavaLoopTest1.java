a4c42cf912d2fb381578416071e1d2b50d436756
masterJKR <nilzero25@gmail.com>
2025-02-06 12:02:15 +0900

    2월 6일 반복문

---
+++ java_study1/src/study0206/JavaLoopTest1.java
@@ -0,0 +1,33 @@
+package study0206;
+
+public class JavaLoopTest1 {
+
+	public static void main(String[] args) {
+		/* 
+		 문제 1 - 무한루프 
+			정수 한개씩  계속 입력 받기  
+			입력 하다가 0 을 입력하면  
+			지금 까지 입력한 모든 숫자의 합 출력
+			int sum=0;
+			while(true){
+				System.out.print("정수 입력 : ");
+				int num = sc.nextInt();
+				if( num==0 ) break;
+				sum += num;
+			}
+			System.out.println(" 총합 : " + sum );
+		
+		 문제 2 - 무한루프
+		 	컴퓨터 와 가위 바위 보 게임 하기
+		 	내가 10번을 이길때 까지 계속 게임하기 
+		 	10번 이기면  그동안 몇번 패배 하고 몇번 비겼는지 출력
+		
+		*/
+
+	}
+
+}
+
+
+
+
