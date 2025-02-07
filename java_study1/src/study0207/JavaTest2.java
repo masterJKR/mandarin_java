0dc08dd38cf6676d111ac24612b5488341427926
masterJKR <nilzero25@gmail.com>
2025-02-07 10:37:30 +0900

    2월 7일 별 찍기

---
+++ java_study1/src/study0207/JavaTest2.java
@@ -0,0 +1,34 @@
+package study0207;
+
+public class JavaTest2 {
+
+	public static void main(String[] args) {
+		/*
+		 문제 4
+		 	*
+		 	**
+		 	***     
+		 	별을 위 와 같이 출력하세요 
+		 	힌트> 첫번째 줄이라서 별한개 두번째 줄이라서 별두개
+		 	     세번째 줄이라서 별세개 ( 이중 for문)
+		 	 for(int line=1; line<=3; line++){
+		 	 	for(int star=1; star<=line; star++){
+		 	 		System.out.print("*");
+		 	 	}		 	 	
+		 	 	System.out.println();
+		 	 }
+		 
+		  문제 5
+		  	0 1 2 3
+		  	4 5 6 7
+		  	8 9 10 11
+		  	숫자를 위와 같이 출력하세요  ( 이중 for문)
+		 */
+
+	}
+
+}
+
+
+
+
