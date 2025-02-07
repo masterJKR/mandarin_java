0ceefd9e5a038500f98320af31964055468f8901
masterJKR <nilzero25@gmail.com>
2025-02-07 12:10:00 +0900

    2월 7일 문제 4,5,6 풀이

--- java_study1/src/study0207/JavaTest2.java
+++ java_study1/src/study0207/JavaTest2.java
@@ -40,7 +40,26 @@ public class JavaTest2 {
 		  	   16566이다.   예) 1234 + 4321 
 		  	6. 위 조건을 모두 충족하는 비밀번호  출력
 		 */
-
+		for(int a=1; a<=9; a++) {
+			for(int b=1; b<=9; b++) {
+				for(int c=1; c<=9; c++) {
+					for(int d=1; d<=9; d++) {
+						if(a!=b && a!=c && a!=d && b!=c && b!=d && c!=d) {
+							if( a == d+2) {
+								if( b < c) { // 두번째 숫자가 세번째보다 작다
+									int pw = a*1000+b*100+c*10+d;
+									int rPw = d*1000+c*100+b*10+a;
+									if( (pw+rPw) == 16566) {
+										System.out.println( pw );
+										break;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 
 }
