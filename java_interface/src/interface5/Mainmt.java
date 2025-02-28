d76c1ccd8f8bd94b90bb61dab90c105d17bd29a3
masterJKR <nilzero25@gmail.com>
2025-02-28 16:12:24 +0900

    2월 28일 인터페이스와 상속

--- java_interface/src/interface5/Mainmt.java
+++ java_interface/src/interface5/Mainmt.java
@@ -4,6 +4,13 @@ public class Mainmt {
 
 	public static void main(String[] args) {
 		
+		CRUD member = new MemberService();
+		CRUD board = new BoardService();
+		
+		member.save();
+		
+		DBConnect mem = new MemberService();
+		//mem.save();  // 부모클래스인 DBConnect에는 save 메서드가 없다.
 	}
 
 }
