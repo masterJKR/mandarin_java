ebdf9efa82be7921fa1c4bccb570b55f1e81e947
masterJKR <nilzero25@gmail.com>
2025-02-28 15:19:13 +0900

    2월 28일 인터펭스 ,  DBConnect

---
+++ java_interface/src/interface4/RemoteCon.java
@@ -0,0 +1,7 @@
+package interface4;
+
+public interface RemoteCon {
+	
+	public void turnOn(); //켜고
+	public void turnOff(); //끄고
+}
---
+++ java_interface/src/interface5/Mainmt.java
@@ -0,0 +1,12 @@
+package interface5;
+
+public class Mainmt {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
+
+
+// 패키지 와 main메서드 있는 클래스 새로만드세요
